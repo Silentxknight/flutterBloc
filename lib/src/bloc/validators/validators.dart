c00f9543689579829ef4d4c9feeeef95e8a2011f
Somyajyoti Das <officialdevco@gmail.com>
2022-04-06 21:41:59 +0530

    Update validators.dart

--- lib/src/bloc/validators/validators.dart
+++ lib/src/bloc/validators/validators.dart
@@ -1,7 +1,21 @@
 import 'dart:async';
 class Validators{
-  final validateEmail= StreamTransformer.fromHandlers(
-    
+  final validateEmail= StreamTransformer<String,String>.fromHandlers(
+    handleData: (email, sink){
+      if(email.contains('@')){
+        sink.add(email);
+      }else{
+        sink.addError('Enter A Valid Email');
+      }
+    } 
   );
-  final validatePass= StreamController();
+  final validatePass=StreamTransformer<String,String>.fromHandlers(
+    handleData: (password, sink){
+      if(password > 3){
+        sink.add(password);
+      }else{
+        sink.addError('Enter A Valid PAssword');
+      }
+    } 
+  );
 }
